3a1062bc18964998047c33841194e2388dfb712e
Andreson Souza <andresonjazz@gmail.com>
2020-08-18 23:47:36 -0300

    feat: refatore o código

--- js/index.js
+++ js/index.js
@@ -1,24 +1,16 @@
-console.log("Arquivo javascript1");
-
 var titulo = document.querySelector(".titulo");
-console.log(titulo);
-console.log(titulo.textContent);
-
 titulo.textContent = "Aparecida Nutricionista";
 
-console.log(document.querySelector("h2"));
-
-var subtitulo = document.querySelector(".subtitulo");
-console.log(subtitulo.textContent);
-
-
 var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = document.querySelector('.info-peso');
-var tdAltura = document.querySelector('.info-altura');
 
-peso = tdPeso.textContent;
-altura = tdAltura.textContent;
+var tdPeso = paciente.querySelector('.info-peso');
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector('.info-altura');
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector('.info-imc');
 
 var imc = peso / Math.pow(altura, 2);
 
-console.log(imc);
+tdImc.textContent = imc;
