11015674c39e00ac3e48347ff608532ae592db4b
Andreson Souza <andresonjazz@gmail.com>
2020-09-22 23:51:43 -0300

    feat: utilize o for para calcular o imc

--- js/principal.js
+++ js/principal.js
@@ -1,35 +1,42 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector('.info-peso');
-var tdAltura = paciente.querySelector('.info-altura');
-var tdImc = paciente.querySelector('.info-imc');
+for (var i = 0; i < pacientes.length; i++) {
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+  var paciente = pacientes[i];
 
+  var tdPeso = paciente.querySelector('.info-peso');
+  var peso = tdPeso.textContent;
+  
+  var tdAltura = paciente.querySelector('.info-altura');
+  var altura = tdAltura.textContent;
+  
+  var tdImc = paciente.querySelector('.info-imc');
 
-var alturaValida = true;
-var pesoValido = true;
+  var pesoValido = true;
+  var alturaValida = true;
 
-if (peso <= 0 || peso >= 1000) {
+  if (peso <= 0 || peso >= 1000) {
     console.log('Peso inválido');
     tdPeso.textContent = 'Peso inválido';
     pesoValido = false;
-};
+  };
 
-if (altura <= 0 || altura >= 3.00) {
+  if (altura <= 0 || altura >= 3.00) {
     console.log('Altura inválida');
     tdAltura.textContent = 'Altura inválida';
     alturaValida = false;
-};
+  };
 
-if (pesoValido && alturaValida) {
+  if (pesoValido && alturaValida) {
 
     var imc = peso / Math.pow(altura, 2);
-    tdImc.textContent = imc;
-} else {
+    tdImc.textContent = imc.toFixed(2);
+  } else {
     tdImc.textContent = "Altura e/ou peso inválidos!"
-};
+  };
+
+}
+
